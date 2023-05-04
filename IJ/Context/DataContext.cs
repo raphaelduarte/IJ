812e4637555a88ea28eb91ea09d8f7ad1efd1e75
Raphael Duarte <raphaelduartessph@gmail.com>
2023-05-04 18:17:24 -0300

    Conclusão de um teste de CREATE no Program.

--- IJ/Context/DataContext.cs
+++ IJ/Context/DataContext.cs
@@ -1,14 +1,19 @@
-﻿using IJ.Entities.Interface;
-using IJ.Entities.Service;
+﻿using IJ.Entities.Service;
 using Microsoft.EntityFrameworkCore;
 
 namespace IJ.Context;
 
 public class DataContext : DbContext
 {
-    public DbSet<Usuario>? Usuarios { get; set; }
+    
+     public DbSet<Usuario>? Usuarios { get; set; }
     public DbSet<Cpf>? Cpfs { get; set; }
     public DbSet<Telefone>? Telefones { get; set; }
     public DbSet<NomeCompleto>? NomesCompletos { get; set; }
-    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Endereco>? Enderecos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=SQLEXPRESS;Database=Investigation Job;Integrated Security=SSPI");
+    }
 }
